8638dfcc815f87ab6618cc12fb1cc83862f7a764
Ran Tao <ran@ephexi.com>
2013-03-02 13:37:37 -0800

    Initial Commit

---
+++ SkySpy/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SkySpy
+//
+//  Created by Ran Tao on 3.2.13.
+//  Copyright (c) 2013 Ran Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
