9c75cf63250431615ec1b518f0c5fc2726053709
Ran Tao <ran@ephexi.com>
2013-03-04 03:18:40 -0800

    add location region monitoring for message drop location

--- SkySpy/MessageMapViewController.m
+++ SkySpy/MessageMapViewController.m
@@ -9,9 +9,15 @@
 #import "MessageMapViewController.h"
 #import "MapAnnotation.h"
 
-@interface MessageMapViewController () <MKMapViewDelegate>
-
+@interface MessageMapViewController () <MKMapViewDelegate, CLLocationManagerDelegate>
+@property (nonatomic, strong) CLLocationManager *locationManager;
+@property (nonatomic) CLLocationCoordinate2D dropPoint;
+@property (nonatomic) CLLocationDistance radius;
+@property (nonatomic, strong) NSString *dropPointName;
 @end
+
+
+
 
 @implementation MessageMapViewController
 
@@ -35,6 +41,44 @@
     messageAnnotation.coordinate = self.mapCenter;
     [self.messageMapView addAnnotation:messageAnnotation];
     [self.messageMapView setRegion:region animated:NO];
+    
+    // Start region monitoring
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+    
+    if ([CLLocationManager regionMonitoringAvailable] &&
+        ([CLLocationManager authorizationStatus] == kCLAuthorizationStatusAuthorized ||
+         [CLLocationManager authorizationStatus] == kCLAuthorizationStatusNotDetermined)) {
+            CLLocationDistance radius = self.radius; // 10 meter radius
+            CLRegion *region = [[CLRegion alloc] initCircularRegionWithCenter:self.dropPoint
+                                                                       radius:radius
+                                                                   identifier:self.dropPointName];
+            [self.locationManager startMonitoringForRegion:region];
+        }
+    
+}
+
+
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
+{
+    if ([region.identifier isEqualToString:self.dropPointName])
+    {
+        // Present camera view controller
+    }
+}
+
+- (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
+{
+    if ([region.identifier isEqualToString:self.dropPointName])
+    {
+        // Remove camera view controller
+    }
+}
+
+
+- (IBAction)backButtonPressed:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning
@@ -43,7 +87,4 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)backButtonPressed:(UIButton *)sender {
-    [self dismissViewControllerAnimated:YES completion:nil];
-}
 @end
